e95ed541371d1cf8d8ae85163703498ede0fd047
CSS <rahamathgousiya@gmail.com>
2024-11-01 19:38:13 +0530

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Login from './Login';
 
 function App() {
   return (
-    <div className="App">
       <div>
-        rahamath
+        <Login/>
       </div>
-    </div>
   );
 }
 
